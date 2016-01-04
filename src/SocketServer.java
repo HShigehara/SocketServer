34f8bde41307eea97364648b6d7d118d921a6403
HShigehara <chilly_0427@hotmail.co.jp>
2016-01-04 15:21:30 +0900

    ファイルの読み込みメソッドを追加

--- src/SocketServer.java
+++ src/SocketServer.java
@@ -2,8 +2,12 @@
  * SocketServer.java
  * サーバークラス．EV3からの通信をPC側で受け取る
  */
+import java.io.*;
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
@@ -18,6 +22,10 @@ public class SocketServer {
     static OutputStream Os; //出力
     static ServerSocket ss; //サーバソケット
 
+    public double velocity;
+    public double yaw;
+    
+    //サーバーのメイン
 	public static void main(String arg[]) {
 		try {
 			System.out.println("Server Starting...");
@@ -30,8 +38,29 @@ public class SocketServer {
 			dos = new DataOutputStream(Os);
 			System.out.println("Socket Connected!");
 			
+			//ここからファイルを読み込む
+			readFile(); //ファイルを読み込むメソッドを呼び出す
+			//
+			
 			//dos.writeInt(6);;
 		}catch(Exception e) {
+			System.out.println("Exception: " + e);
+		}
+	}
+	
+	//ファイル読み込み用のメソッド
+	public static void readFile(){
+		try{
+			SocketServer ss = new SocketServer();
+			FileReader fr = new FileReader("test.dat");
+			StreamTokenizer st = new StreamTokenizer(fr);
+			st.nextToken();
+			ss.velocity = st.nval;
+			st.nextToken();
+			ss.yaw = st.nval;
+			System.out.println("v = " + ss.velocity + "Yaw = " + ss.yaw);
+			fr.close();
+		}catch(IOException e){
 			System.out.println("Exception: " + e);
 		}
 	}
